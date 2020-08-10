54d53fcdcff246bb6d3091ae09c742a612ce2b3b
Emmanuel <akhigbemmanuel@gmail.com>
2020-08-11 00:26:11 +0100

    Add vue-router

--- src/App.vue
+++ src/App.vue
@@ -1,78 +1,32 @@
 <template>
   <div id="app">
-    <Header />
-    <AddTodo @add-todo="addTodo" />
-    <div>
-      <Todos v-bind:todos="todos" @del-todo="deleteTodo" />
+    <div id="nav">
+      <router-link to="/">Home</router-link> |
+      <router-link to="/about">About</router-link>
     </div>
+    <router-view />
   </div>
 </template>
 
-<script>
-  import Todos from "./components/Todos";
-  import AddTodo from "./components/AddTodo";
-  import Header from "./components/layout/Header";
-  import axios from "axios";
-
-  export default {
-    name: "App",
-    components: {
-      Todos,
-      Header,
-      AddTodo,
-    },
-    data() {
-      return {
-        todos: [],
-      };
-    },
-    methods: {
-      deleteTodo(todoId) {
-        axios
-          .delete(`https://jsonplaceholder.typicode.com/todos/${todoId}`)
-          .then(
-            () => (this.todos = this.todos.filter(({ id }) => id !== todoId))
-          )
-          .catch((err) => alert(err.toString()));
-      },
-      addTodo({ title, completed }) {
-        axios
-          .post(`https://jsonplaceholder.typicode.com/todos`, {
-            title,
-            completed,
-          })
-          .then(({ data }) => (this.todos = [...this.todos, data]))
-          .catch((err) => alert(err.toString()));
-      },
-    },
-    created() {
-      axios
-        .get(`https://jsonplaceholder.typicode.com/todos?_limit=5`)
-        .then(({ data }) => (this.todos = data))
-        .catch((err) => alert(err.toString()));
-    },
-  };
-</script>
-
 <style>
-  * {
-    box-sizing: border-box;
-    margin: 0;
-    padding: 0;
+  #app {
+    font-family: Avenir, Helvetica, Arial, sans-serif;
+    -webkit-font-smoothing: antialiased;
+    -moz-osx-font-smoothing: grayscale;
+    text-align: center;
+    color: #2c3e50;
   }
-  body {
-    font-family: Raleway, Arial, Helvetica, sans-serif;
-    line-height: 1.4;
+
+  #nav {
+    padding: 30px;
   }
-  .btn {
-    display: inline-block;
-    border: none;
-    background: #555;
-    color: #fff;
-    padding: 7px 20px;
-    cursor: pointer;
+
+  #nav a {
+    font-weight: bold;
+    color: #2c3e50;
   }
-  .btn:hover {
-    background: #666;
+
+  #nav a.router-link-exact-active {
+    color: #42b983;
   }
 </style>
---
+++ src/views/Home.vue
@@ -0,0 +1,78 @@
+<template>
+  <div id="app">
+    <Header />
+    <AddTodo @add-todo="addTodo" />
+    <div>
+      <Todos v-bind:todos="todos" @del-todo="deleteTodo" />
+    </div>
+  </div>
+</template>
+
+<script>
+  import Todos from "@/components/Todos";
+  import AddTodo from "@/components/AddTodo";
+  import Header from "@/components/layout/Header";
+  import axios from "axios";
+
+  export default {
+    name: "Home",
+    components: {
+      Todos,
+      Header,
+      AddTodo,
+    },
+    data() {
+      return {
+        todos: [],
+      };
+    },
+    methods: {
+      deleteTodo(todoId) {
+        axios
+          .delete(`https://jsonplaceholder.typicode.com/todos/${todoId}`)
+          .then(
+            () => (this.todos = this.todos.filter(({ id }) => id !== todoId))
+          )
+          .catch((err) => alert(err.toString()));
+      },
+      addTodo({ title, completed }) {
+        axios
+          .post(`https://jsonplaceholder.typicode.com/todos`, {
+            title,
+            completed,
+          })
+          .then(({ data }) => (this.todos = [...this.todos, data]))
+          .catch((err) => alert(err.toString()));
+      },
+    },
+    created() {
+      axios
+        .get(`https://jsonplaceholder.typicode.com/todos?_limit=5`)
+        .then(({ data }) => (this.todos = data))
+        .catch((err) => alert(err.toString()));
+    },
+  };
+</script>
+
+<style>
+  * {
+    box-sizing: border-box;
+    margin: 0;
+    padding: 0;
+  }
+  body {
+    font-family: Raleway, Arial, Helvetica, sans-serif;
+    line-height: 1.4;
+  }
+  .btn {
+    display: inline-block;
+    border: none;
+    background: #555;
+    color: #fff;
+    padding: 7px 20px;
+    cursor: pointer;
+  }
+  .btn:hover {
+    background: #666;
+  }
+</style>
