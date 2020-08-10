47105dfbfd6905aa52c2a8251b017ed1a47825e9
Emmanuel <akhigbemmanuel@gmail.com>
2020-08-10 23:04:30 +0100

    set dummy task id with uuid

--- src/components/AddTodo.vue
+++ src/components/AddTodo.vue
@@ -2,39 +2,48 @@
   <div>
     <form @submit="addTodo">
       <label for="todoTitle">Title</label>
-      <input type="text" v-model="title" id="todoTitle" placeholder="Enter todo" class="todo-input" />
+      <input
+        type="text"
+        v-model="title"
+        id="todoTitle"
+        placeholder="Enter todo"
+        class="todo-input"
+      />
       <input type="submit" value="Submit" class="btn" />
     </form>
   </div>
 </template>
 
 <script>
-export default {
-  name: "AddTodo",
-  data() {
-    return {
-      title: "",
-    };
-  },
-    methods:{
-        addTodo(){
-            const newTodo = {
-                id:
-            }
-        }
-    }
-};
+  import uuid from "uuid";
+  export default {
+    name: "AddTodo",
+    data() {
+      return {
+        title: "",
+      };
+    },
+    methods: {
+      addTodo() {
+        const newTodo = {
+          id: uuid.v4(),
+          title: this.title,
+          completed: false,
+        };
+      },
+    },
+  };
 </script>
 
 <style scoped>
-form {
-  display: flex;
-}
-.todo-input {
-  flex: 10;
-  padding: 3px 2px;
-}
-.btn {
-  flex: 2;
-}
+  form {
+    display: flex;
+  }
+  .todo-input {
+    flex: 10;
+    padding: 3px 2px;
+  }
+  .btn {
+    flex: 2;
+  }
 </style>
