66c41296c9a06d8d42d22651bc62077d3726cf84
Emmanuel <akhigbemmanuel@gmail.com>
2020-08-10 19:40:01 +0100

    clear boilerplate code

--- src/App.vue
+++ src/App.vue
@@ -5,23 +5,20 @@
 </template>
 
 <script>
-import HelloWorld from "./components/HelloWorld.vue";
-
 export default {
   name: "App",
-  components: {
-    HelloWorld,
-  },
+  components: {},
 };
 </script>
 
 <style>
-#app {
-  font-family: Avenir, Helvetica, Arial, sans-serif;
-  -webkit-font-smoothing: antialiased;
-  -moz-osx-font-smoothing: grayscale;
-  text-align: center;
-  color: #2c3e50;
-  margin-top: 60px;
+* {
+  box-sizing: border-box;
+  margin: 0;
+  padding: 0;
+}
+body {
+  font-family: Raleway, Arial, Helvetica, sans-serif;
+  line-height: 1.4;
 }
 </style>
